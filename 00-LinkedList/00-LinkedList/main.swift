342872c34418d666bb0e03288a89f1dbf559b271
sanghyeok-kim <slsnsi1212@gmail.com>
2022-01-29 17:05:20 +0900

    Rename

--- 00-LinkedList/00-LinkedList/main.swift
+++ 00-LinkedList/00-LinkedList/main.swift
@@ -14,29 +14,38 @@ let node1 = Node(data: 1)
 let node2 = Node(data: 2)
 let node3 = Node(data: 3)
 
-let myDLL = DoubleLinkedList()
+let myDLL = DoublyLinkedList<Int>()
 
 myDLL.showAll()
 
-myDLL.insertLast(newNode: node1)
+myDLL.insertAtLast(newNode: node1)
 myDLL.showAll()
 
-myDLL.insertLast(newNode: node2)
+myDLL.insertAtLast(newNode: node2)
 myDLL.showAll()
 
-myDLL.insertLast(newNode: node3)
+myDLL.insertAtLast(newNode: node3)
 myDLL.showAll()
 
 let node4 = Node(data: 4)
 myDLL.insertAfter(targetNodeData: 3, newNode: node4)
 myDLL.showAll()
 
-//let node5 = Node(data: 5)
-//myDLL.insertAfter(targetNodeData: 4, newNode: node5)
+
+//myDLL.deleteNode(targetNodeData: 4)
 //myDLL.showAll()
 
-//myDLL.searchNodeFromHead(by: <#T##Int#>)
+//myDLL.deleteNode(targetNodeData: 2)
+//myDLL.showAll()
 
-myDLL.deleteNode(targetNodeData: 1)
+//myDLL.deleteNode(targetNodeData: 2)
+//myDLL.showAll()
+
+//myDLL.deleteNode(targetNodeData: 3)
+//myDLL.showAll()
+
+//myDLL.deleteLastNode()
+//myDLL.showAll()
+
+myDLL.swapNode(leftNode: node2, rightNode: node3)
 myDLL.showAll()
-
