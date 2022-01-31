99c2186e5a7545057e8598bce994dcb8f0577ab8
sanghyeok-kim <slsnsi1212@gmail.com>
2022-01-31 23:32:48 +0900

    SLL, CLL Test

--- 00-LinkedList/00-LinkedList/main.swift
+++ 00-LinkedList/00-LinkedList/main.swift
@@ -1,51 +1,110 @@
-/* ----- Single-LinkedList test ----- */
+/* ----- 1. Single-LinkedList test ----- */
+//let node1 = SLLNode(data: 1)
+//let node2 = SLLNode(data: 2)
+//let node3 = SLLNode(data: 3)
+//let mySLL = SinglyLinkedList<Int>()
+//
+//mySLL.showAll()
+//
+//mySLL.insertAtFirst(newNode: node1)
+//mySLL.showAll()
+//
+//
+//mySLL.insertBefore(targetNodeData: 1, newNode: node2)
+//mySLL.showAll()
+//
+//mySLL.remove(targetNodeData: 2)
+//mySLL.showAll()
+//
+//mySLL.remove(targetNodeData: 1)
+//mySLL.showAll()
 
 
 
+/* ----- 2. Circular-LinkedList test ----- */
+let myCLL = CircularLinkedList<Int>()
+
+let node1 = CLLNode(data: 1)
+let node2 = CLLNode(data: 2)
+let node3 = CLLNode(data: 3)
+
+myCLL.insertAtLast(newNode: node1)
+//myCLL.insertAtFirst(newNode: node1)
+myCLL.showAll()
+
+myCLL.insertAtLast(newNode: node2)
+//myCLL.insertAtFirst(newNode: node2)
+myCLL.showAll()
+
+myCLL.insertAtLast(newNode: node3)
+//myCLL.insertAtFirst(newNode: node3)
+myCLL.showAll()
+
+//print(myCLL.searchPrevNode(targetNodeData: 1)?.data)
+//print(myCLL.searchNode(targetNodeData: 4)?.data)
+
+//myCLL.removeFirst()
+//myCLL.removeLast()
+//myCLL.remove(targetNodeData: 1)
+//myCLL.showAll()
+
+//myCLL.removeFirst()
+//myCLL.removeLast()
+//myCLL.remove(targetNodeData: 2)
+//myCLL.showAll()
 
 
+//myCLL.removeFirst()
+//myCLL.removeLast()
+//myCLL.remove(targetNodeData: 3)
+//myCLL.showAll()
 
 
-
-
-
-/* ----- Double-LinkedList test ----- */
-let node1 = Node(data: 1)
-let node2 = Node(data: 2)
-let node3 = Node(data: 3)
-
-let myDLL = DoublyLinkedList<Int>()
-
-myDLL.showAll()
-
-myDLL.insertAtLast(newNode: node1)
-myDLL.showAll()
-
-myDLL.insertAtLast(newNode: node2)
-myDLL.showAll()
-
-myDLL.insertAtLast(newNode: node3)
-myDLL.showAll()
-
-let node4 = Node(data: 4)
-myDLL.insertAfter(targetNodeData: 3, newNode: node4)
-myDLL.showAll()
-
-
-//myDLL.deleteNode(targetNodeData: 4)
+/* ----- 3. Double-LinkedList test ----- */
+//let node1 = DLLNode(data: 1)
+//let node2 = DLLNode(data: 2)
+//let node3 = DLLNode(data: 3)
+//
+//let myDLL = DoublyLinkedList<Int>()
+//
+//myDLL.showAll()
+//
+//myDLL.insertAtLast(newNode: node1)
+//myDLL.showAll()
+//
+//myDLL.insertAtLast(newNode: node2)
+//myDLL.showAll()
+//
+//myDLL.insertAtLast(newNode: node3)
+//myDLL.showAll()
+//
+//let node4 = DLLNode(data: 4)
+//myDLL.insertAfter(targetNodeData: 3, newNode: node4)
+//myDLL.showAll()
+//
+//
+//myDLL.removeNode(targetNodeData: 4)
+//myDLL.showAll()
+//
+//myDLL.removeNode(targetNodeData: 2)
+//myDLL.showAll()
+//
+//myDLL.removeNode(targetNodeData: 2)
+//myDLL.showAll()
+//
+//myDLL.removeNode(targetNodeData: 3)
+//myDLL.showAll()
+//
+//myDLL.removeLastNode()
+//myDLL.showAll()
+//
+//myDLL.swapNode(leftNode: node2, rightNode: node3)
 //myDLL.showAll()
 
-//myDLL.deleteNode(targetNodeData: 2)
-//myDLL.showAll()
 
-//myDLL.deleteNode(targetNodeData: 2)
-//myDLL.showAll()
+var s = ""
+let a = 1
+s += String("\(a)asd , ")
+s += String("\(a)asd :> ")
+print(s)
 
-//myDLL.deleteNode(targetNodeData: 3)
-//myDLL.showAll()
-
-//myDLL.deleteLastNode()
-//myDLL.showAll()
-
-myDLL.swapNode(leftNode: node2, rightNode: node3)
-myDLL.showAll()
