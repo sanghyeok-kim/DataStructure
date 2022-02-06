6847ab5071f8a39873b1d2088f33e29d0ce1cb5e
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-07 01:22:20 +0900

    MinHeap

--- 04-Heap/04-Heap/1-MinHeap.swift
+++ 04-Heap/04-Heap/1-MinHeap.swift
@@ -1,164 +1,178 @@
-////
-////  1-MinHeap.swift
-////  04-Heap
-////
-////  Created by 김상혁 on 2022/02/05.
-////
+
 //
-//import Foundation
+//  1-MinHeap.swift
+//  04-Heap
 //
-////enum ChildState {
-////    case none   //child가 없음
-////    case left   //left child만 있음
-////    case right  //left, right child 둘 다 있음
-////}
+//  Created by 김상혁 on 2022/02/05.
 //
-//struct Element<T: Comparable> {
-//    let data: T
-//}
-//
-//struct MinHeap<T: Comparable> {
-//    var heap: [Element<T>] = []
-//
-//    init() {}
-//    init(root: Element<T>) {
-//        heap.append(root) //0번 index 채우기용
-//        heap.append(root) //실제 Root Node 채우기
-//    }
-//
-//    //Get Index
-//    func getLeftChildIndex(of parentIndex: Int) -> Int {
-//        return 2 * parentIndex
-//    }
-//    func getRightChildIndex(of parentIndex: Int) -> Int {
-//        return 2 * parentIndex + 1
-//    }
-//
-//    func getParentIndex(of childIndex: Int) -> Int {
-//        return childIndex / 2
-//    }
-//
-//    //Get Element
-//    func getLeftChild(of parentIndex: Int) -> Element<T> {
-//        return heap[getLeftChildIndex(of: parentIndex)]
-//    }
-//
-//    func getRightChild(of parentIndex: Int) -> Element<T> {
-//        return heap[getRightChildIndex(of: parentIndex)]
-//    }
-//
-//    func getParent(of childIndex: Int) -> Element<T> {
-//        return heap[getParentIndex(of: childIndex)]
-//    }
-//
-//
-//    //Boolean Check
-//    func hasLeftChild(of index: Int) -> Bool {
-//        return getLeftChildIndex(of: index) < heap.count
-//    }
-//
-//    func hasRightChild(of index: Int) -> Bool {
-//        return getRightChildIndex(of: index) < heap.count
-//    }
-//
-//    func hasParent(of index: Int) -> Bool {
-//        return getParentIndex(of: index) > 1
-//    }
-//
-//
-//    //swap
-//    mutating func swap(indexOne: Int, indexTwo: Int) {
-//        let placeholder = self.heap[indexOne]
-//        self.heap[indexOne] = self.heap[indexTwo]
-//        self.heap[indexTwo] = placeholder
-//    }
-//
-//
-//    mutating func push(_ data: Element<T>) {
-//        if heap.count == 0 {
-//            heap.append(data)
-//            heap.append(data)
-//            return
-//        }
-//
-//        heap.append(data)
-//    }
-//
-//    mutating func pop() -> Element<T>? {
-//        if heap.count > 1 {
-//            let returnElement =  heap[1]
-//
-//            //heapifyDown()
-//
-//
-//            return returnElement
-//        }
-//        else {
-//            return nil
-//        }
-//    }
-//
-//    mutating func peek() -> Element<T>? {
-//        if heap.count > 1 {
-//            let returnElement =  heap[1]
-//
-//
-//
-//            return returnElement
-//        }
-//        else {
-//            return nil
-//        }
-//    }
-//}
-//
-////    func heapifyDown(from startIndex: T) -> ChildState {
-////        let leftChildIndex = (startIndex * 2)
-////        let rightChildIndex = leftChildIndex + 1
-////
-////
-////        if hasLeftChild(of: startIndex) == false {
-////            return .none
-////        }
-////
-////
-////        if hasLeftChild(of: startIndex) {
-////            return getLeftChild(of: startIndex) > heap[startIndex] ? .left : .none
-////        }
-////
-////
-////        if hasRightChild(of: startIndex) {
-////            if getLeftChildIndex(of: startIndex) > heap[startIndex] &&
-////                getRightChildIndex(of: startIndex) > heap[startIndex]
-////            return .none
-////        }
-////
-////
-////        if (heap[leftChildIndex] > heap[startIndex]) && (heap[rightChildIndex] > heap[startIndex]) {
-////            return heap[leftChildIndex] > heap[rightChildIndex] ? .left : .right
-////        }
-////
-////
-////        return heap[leftChildIndex] > heap[startIndex] ? .left : .right
-////    }
-////}
-//
-//let elementA = Element(data: 1)
-//let elementB = Element(data: 2)
-//let elementC = Element(data: 3)
-//
-//
-//var heap = MinHeap(root: elementA)
-//print(heap)
-////print("")
-//
-////h.push(elementB)
-////print(h.heap)
-////print("")
-////
-////h.push(elementC)
-////print(h.heap)
-////print("")
-//
-////print(h.hasRightChild(of: 1))
-////print(h.getLeftChildElement(of: 2))
+
+import Foundation
+
+enum ChildState {
+    case none
+    case left
+    case both
+}
+
+struct Element<T: Comparable> {
+    let data: T
+}
+
+struct MinHeap<T: Comparable> {
+    var heap: [Element<T>] = []
+    let rootIndex = 1
+    
+    init() {}
+    init(root: Element<T>) {
+        heap.append(root) //0번 index 채우기용
+        heap.append(root) //실제 Root Node 채우기
+    }
+    
+    //Get Index
+    func getLeftChildIndex(of parentIndex: Int) -> Int? {
+        if parentIndex < 1 || parentIndex * 2 >= heap.count {
+            return nil
+        }
+        else {
+            return parentIndex * 2
+        }
+    }
+    func getRightChildIndex(of parentIndex: Int) -> Int? {
+        if parentIndex < 1 || parentIndex * 2 + 1 >= heap.count {
+            return nil
+        }
+        else {
+            return parentIndex * 2 + 1
+        }
+    }
+    
+    func getParentIndex(of childIndex: Int) -> Int? { //(2...heap.count - 1)
+        if childIndex <= 1 || childIndex >= heap.count {
+            return nil
+        }
+        else {
+            return childIndex / 2
+        }
+    }
+    
+    mutating func swap(indexOne: Int, indexTwo: Int) {
+        let placeholder = self.heap[indexOne]
+        self.heap[indexOne] = self.heap[indexTwo]
+        self.heap[indexTwo] = placeholder
+    }
+    
+    
+    mutating func push(_ newNode: Element<T>) {
+        if heap.count == 0 {
+            heap.append(newNode)
+            heap.append(newNode)
+            return
+        }
+        else {
+            heap.append(newNode)
+        }
+        
+        
+        func isMoveUp(_ insertIndex: Int) -> Bool { //(insertIndex가 root가 아님 && insert의 data가 parent의 data 보다 큼) -> true
+            if insertIndex <= 1 {
+                return false
+            }
+            else {
+                guard let parentIndex = getParentIndex(of: insertIndex) else { return false }
+                return heap[parentIndex].data > heap[insertIndex].data
+            }
+        }
+        
+        var insertIndex = heap.count - 1
+        while isMoveUp(insertIndex) {
+            guard let parentIndex = getParentIndex(of: insertIndex) else { return }
+            heap.swapAt(insertIndex, parentIndex)
+            insertIndex = parentIndex
+        }
+    }
+    
+    mutating func pop() -> Element<T>? {
+        if heap.count <= 1 { return nil }
+        
+        let returnElement =  heap[rootIndex]
+        
+        heap.swapAt(rootIndex, heap.count - 1)
+        heap.removeLast()
+        
+        func checkChildState(of targetIndex: Int) -> ChildState {
+            if getLeftChildIndex(of: targetIndex) == nil {
+                return .none
+            }
+            else if getLeftChildIndex(of: targetIndex) != nil && getRightChildIndex(of: targetIndex) == nil {
+                return .left
+            }
+            else {
+                return .both
+            }
+        }
+        
+        func heapifyDown(from startIndex: Int) {
+            switch checkChildState(of: startIndex) {
+                
+            //왼쪽 자식 노드가 없는 경우(== 자식 노드가 없는 경우 == leaf 노드인 경우) -> heapify 종료
+            case .none:
+                return
+                
+            //왼쪽 자식 노드만 있는 경우
+            case .left:
+                guard let leftIndex = getLeftChildIndex(of: startIndex) else { return }
+                
+                //왼쪽 자식이 나보다 크거나 같다면 -> heapify 종료
+                if heap[leftIndex].data >= heap[startIndex].data {
+                    return
+                }
+                
+                //왼쪽 자식이 나보다 작으면 -> swap 후 다시 heapify
+                else {
+                    swap(indexOne: leftIndex, indexTwo: startIndex)
+                    heapifyDown(from: leftIndex)
+                }
+                
+            //왼쪽 & 오른쪽 자식 노드 모두 있는 경우
+            case .both:
+                guard let leftIndex = getLeftChildIndex(of: startIndex) else { return }
+                guard let rightIndex = getRightChildIndex(of: startIndex) else { return }
+                
+                //두 자식 모두 나보다 크거나 같다면 -> heapify 종료
+                if heap[leftIndex].data >= heap[startIndex].data && heap[rightIndex].data >= heap[startIndex].data {
+                    return
+                }
+                
+                //둘 다 자신보다 작다면, 둘 중에 더 작은 자식과 swap 후 다시 heapify
+                else if heap[leftIndex].data < heap[startIndex].data && heap[rightIndex].data >= heap[startIndex].data {
+                    swap(indexOne: leftIndex, indexTwo: startIndex)
+                    heapifyDown(from: leftIndex)
+                }
+                else if heap[leftIndex].data >= heap[startIndex].data && heap[rightIndex].data < heap[startIndex].data {
+                    swap(indexOne: rightIndex, indexTwo: startIndex)
+                    heapifyDown(from: rightIndex)
+                }
+                
+                //둘 중 하나만 자신보다 작다면, 그 하나와 자신을 swap 후 다시 heapify
+                else {
+                    let smallerChildIndex = heap[leftIndex].data < heap[rightIndex].data ? leftIndex : rightIndex
+                    swap(indexOne: smallerChildIndex, indexTwo: startIndex)
+                    heapifyDown(from: smallerChildIndex)
+                }
+            }
+        }
+        
+        heapifyDown(from: rootIndex)
+        return returnElement
+    }
+    
+    func peek() -> Element<T>? {
+        if heap.count <= 1 {
+            return nil
+        }
+        else {
+            let returnElement =  heap[rootIndex]
+            return returnElement
+        }
+    }
+}
